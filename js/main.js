188ca55eeb268db81f0eae465842bfc5cdc36f02
dunndealpro <dunn.evan.f@gmail.com>
2022-10-15 15:03:01 -0700

    add variables and constants to JS file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-console.log('is this working')
+console.log("Connected to html file!")
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+let dealerHand,
+    playerHand,
+    playerName,
+    playerPhoto,
+    wager,
+    insurance,
+    startingBalance,
+    winAmount;
+
+
+/*----- cached elements  -----*/
+// Player Choices
+
+const startButton = document.querySelector('.start')
+const rulesButton = document.querySelector('.rules')
+const wagerButton = document.querySelector('.wager')
+const dealButton = document.querySelector('.deal')
+const hitButton = document.querySelector('.hit')
+const standButton = document.querySelector('.stand')
+const continueButton = document.querySelector('.continue')
+const yesButton = document.querySelector('.yes')
+const noButton = document.querySelector('.no')
+const restartButton = document.querySelector('.restart')
+
+// Display Game Info
+const dealersCards = document.querySelector('.dealers-cards')
+const playersCards = document.querySelector('.players-cards')
+const imageDealer = document.querySelector('.image-dealer')
+const imagePlayer = document.querySelector('.image-player')
+const playerHandInfo = document.querySelector('.player-hand-info')
+const imageCardDeck = document.querySelector('.card-deck')
+const imagePayOut = document.querySelector('.payout')
+
+
+// Reset Game
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
